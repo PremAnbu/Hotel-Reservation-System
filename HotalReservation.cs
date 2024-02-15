e502df1ba170dd8005933250da1631b423573649
PremAnbu <premanbu9360@gmail.com>
2024-02-15 10:36:30 +0530

    first commit welcome page

---
+++ HotalReservation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationSystem
+{
+    public class HotalReservation
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Hotel Reservation ---!");
+            UserChoice userChoice = new UserChoice();
+           // userChoice.Choice();
+        }
+    }
+}
