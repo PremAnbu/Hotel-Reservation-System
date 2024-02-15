cb4167d36814794763f4f156f3ce7286c640d138
PremAnbu <premanbu9360@gmail.com>
2024-02-15 12:39:26 +0530

    Add poco class for Hotal Reservation

---
+++ HotelPocoClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationSystem
+{
+    public class HotelPocoClass
+    {
+        public string HotelName { get; set; }
+        public int HotelRating { get; set; }
+        public int WeekdayRegular { get; set; }
+        public int WeekdayRewards { get; set; }
+        public int WeekendRegular { get; set; }
+        public int WeekendRewards { get; set; }
+
+
+    }
+}
